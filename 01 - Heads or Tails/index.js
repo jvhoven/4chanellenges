a32661d1941dab93044e90429d77be2e2309ed50
Tom Grootjans <tom.grootjans@gmail.com>
2015-12-08 19:45:20 +0100

    jemoer toevoegen

--- 01 - Heads or Tails/index.js
+++ 01 - Heads or Tails/index.js
@@ -1,5 +1,5 @@
-var util = require('./../_util/util');
 var readline = require('readline');
+var util = require('./../_util/util');
 
 var possibilities = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 var randomGenerated = util.rng(12,0);
@@ -9,35 +9,39 @@ var rl = readline.createInterface({
 });
 
 console.log("Pulled card is:"+ pull());
-rl.question("Guess higher or lower: ", function(answer){
-
-    rl.close();
-});
-
+var doQuestion = function(){
+    rl.question("Guess higher or lower: ", function(answer){
+        if (confirm(answer)) {
+            return rl.close();
+        }
+        doQuestion();
+    });
+}
+doQuestion();
 
 function pull() {
     var pick = util.rng(12,0);
     var card = possibilities[pick];
     possibilities.splice(pick, 1);
     return card;
-}
+};
 
 function confirm(answer) {
         if (answer.toLowerCase() == "lower"){
             // lower code
-
+            console.log("lower!");
+            return true;
         } else if (answer.toLowerCase() == "higher") {
             // higher code
+            console.log("higher!");
+            return true;
         } else {
             // error: does not exists
-            console.log("Only enter \"higer\" or \"lower\"");
+            console.log("Only enter \"higher\" or \"lower\"");
         }
-        if(val.toLowerCase() == possibilities[i].toLowerCase()) {
-            return true;
-        }
-    }
+
     return false;
-}
+};
 
 /*
 function exists(val){
